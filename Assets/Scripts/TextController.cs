f1398dde4423a65fe952db077f2cbcf4b7eab1ef
KIMBO-PC\Kimbo <kimpuolakka@gmail.com>
2018-03-21 09:43:17 +0200

    First two states

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Text text;
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
+	private States myState;
+	void Start () {
+		myState = States.cell;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (myState);
+		if (myState == States.cell)
+		{
+			state_cell();
+		}
+		else if (myState == States.sheets_0)
+		{
+			state_sheets_0();
+		}
+	}
+
+	void state_cell ()
+	{
+		text.text = "Dudeman was a mean guy who was in prison. In the prison there was a " +
+					"painting on the wall that just said 'Prison' to remind Mean Dudeman that he was " +
+					"indeed, in prison.\n \n" +
+					"Press 'S' to view Sheets \n" +
+					"Press 'M' to view Mirror \n" +
+					"Press 'L' to view Lock";
+
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			myState = States.sheets_0;
+		}
+	}
+
+		void state_sheets_0 ()
+	{
+		text.text = "You look at your sheets and for a moment think about hanging yourself. It's been a while since you saw the outside world. But today is " +
+					"the day you're getting out and this is no time to despair. You look around the cell for other things once more. \n \n" +
+					"Press 'C' to view Cell \n" +
+					"Press 'M' to view Mirror \n" +
+					"Press 'L' to view Lock";
+
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			myState = States.cell;
+		}
+	}
+}
